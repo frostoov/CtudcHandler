ac32477a67902c47b8fc0211109b45dcc207ccfe
frostoov <frostoov@gmail.com>
2016-07-23 03:25:03 +0300

    Fixed invalid package sequnce handling

--- split.go
+++ split.go
@@ -15,7 +15,7 @@ type RunData struct {
 	writer     *bufio.Writer
 	eventCount int
 	fileCount  int
-	lastRecord int
+	lastRecord uint
 	badRecord  bool
 }
 
@@ -47,48 +47,46 @@ func split(dirnames []string) error {
 		for record.Unmarshal(r) == nil {
 			run := int(record.Nrun())
 			runWriter := runWriters[run]
-			if runWriter == nil {
-				rundir := formatRunDir(run)
-				if err := os.MkdirAll(path.Join(rundir, "ctudc"), 0777); err != nil {
-					return err
-				}
-				filename := formatCtudcFilename(run, 0)
+			if runWriter != nil && runWriter.lastRecord >= record.Nevent() {
+				log.Printf("split previousRecord(%v) >= currentRecord(%v) run #%v\n",
+					runWriter.lastRecord, record.Nevent(), run)
+			} else {
+				if runWriter == nil {
+					rundir := formatRunDir(run)
+					if err := os.MkdirAll(path.Join(rundir, "ctudc"), 0777); err != nil {
+						return err
+					}
+					filename := formatCtudcFilename(run, 0)
 
-				f, err := os.Create(filename)
-				log.Println("Created: ", rundir)
-				if err != nil {
-					return err
+					f, err := os.Create(filename)
+					log.Println("Created: ", rundir)
+					if err != nil {
+						return err
+					}
+					w := bufio.NewWriter(f)
+					w.WriteString("TDSa\n")
+					runWriter = &RunData{
+						file:   f,
+						writer: w,
+					}
+					runWriters[run] = runWriter
+				} else if runWriter.eventCount > 10000 {
+					runWriter.fileCount++
+					runWriter.eventCount = 0
+					filename := formatCtudcFilename(run, runWriter.fileCount)
+					f, err := os.Create(filename)
+					if err != nil {
+						return err
+					}
+					runWriter.file = f
+					w := bufio.NewWriter(f)
+					w.WriteString("TDSa\n")
+					runWriter.writer = w
 				}
-				w := bufio.NewWriter(f)
-				w.WriteString("TDSa\n")
-				runWriter = &RunData{
-					file:       f,
-					writer:     w,
-					eventCount: 0,
-					fileCount:  0,
-				}
-				runWriters[run] = runWriter
-			} else if runWriter.eventCount > 10000 {
-				runWriter.fileCount++
-				runWriter.eventCount = 0
-				filename := formatCtudcFilename(run, runWriter.fileCount)
-				f, err := os.Create(filename)
-				if err != nil {
-					return err
-				}
-				runWriter.file = f
-				w := bufio.NewWriter(f)
-				w.WriteString("TDSa\n")
-				runWriter.writer = w
-			}
-			if runWriter.lastRecord < int(record.Nevent()) {
 				record.Marshal(runWriter.writer)
-				runWriter.lastRecord = int(record.Nevent())
 				runWriter.eventCount++
-			} else if !runWriter.badRecord {
-				log.Printf("split runWriter.lastRecord < int(record.Nevent()) run #%d\n", run)
-				runWriter.badRecord = true
 			}
+			runWriter.lastRecord = record.Nevent()
 		}
 		return nil
 	}
